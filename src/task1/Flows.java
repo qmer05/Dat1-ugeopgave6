bf532f41b1fe355628aabd06fe8b7d706f0507c1
Ömer Øcalan <cph-oo203@cphbusiness.dk>
2023-10-11 12:22:23 +0200

    update

--- src/task1/Flows.java
+++ src/task1/Flows.java
@@ -3,29 +3,39 @@ package task1;
 public class Flows {
 
     public void methodA(String input) {
-        System.out.println("a");
-        if (!input.equals("start")) {
+        System.out.println("J");
+        if (!input.equals("javaErHvad")) {
             System.out.println("b");
-            methodB("start");
+            methodB("javaErHvad");
         } else {
-            System.out.println("d");
-            System.out.println(methodC(input));
+            System.out.println("a");
+            if (methodC(input) == 10) {
+                System.out.println("r");
+            }
         }
-        System.out.println("i");
+        System.out.println("s");
+        System.out.println("j");
+        System.out.println("o");
+        if(input.equals("javaErHvad")){
+            System.out.println("v");
+            System.out.println("t");
+        }
     }
     public void methodB(String start) {
         System.out.println("c");
     }
     public int methodC(String input) {
-        System.out.println("e");
+        System.out.println("v");
         methodD(input.length());
         return input.length() * 2;
     }
+
     public void methodD(int number) {
-        System.out.println("f");
-        if (number > 5) {
+        System.out.println("a");
+        if (number > 11) {
             System.out.println("g");
         }
-        System.out.println("h");
+        System.out.println("e");
+        System.out.println("r");
     }
 }
--- src/task1/Main.java
+++ src/task1/Main.java
@@ -1,2 +1,6 @@
 package task1;public class Main {
+    public static void main(String[] args){
+        Flows flows = new Flows();
+        flows.methodA("javaErHvad");
+    }
 }
