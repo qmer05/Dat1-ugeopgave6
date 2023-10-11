7e866a22ea80f05d25119faeaee6687cbedcdb42
qmer05 <cph-oo203@cphbusiness.dk>
2023-10-11 18:03:48 +0200

    update

--- src/task3/Main.java
+++ src/task3/Main.java
@@ -8,15 +8,30 @@ public class Main {
         ArrayList<String> passedCourses1 = new ArrayList<>();
         passedCourses1.add("english");
 
-        ArrayList<String> currentCourses1 = new ArrayList<>();
-        currentCourses1.add("mathematics");
+        ArrayList<String> studentCurrentCourses1 = new ArrayList<>();
+        studentCurrentCourses1.add("mathematics");
 
-        Person student1 = new Student("Ømer", passedCourses1, currentCourses1);
+        Person student1 = new Student("Ømer", passedCourses1, studentCurrentCourses1);
 
         student1.addCourse("english");
         student1.addCourse("german");
 
-        System.out.println(student1);
+        System.out.println("Student: " + student1.getName() + "\t" + student1 + "\n");
 
+        ArrayList<String> canTeach1 = new ArrayList<>();
+        canTeach1.add("english");
+        canTeach1.add("chemistry");
+        canTeach1.add("geography");
+
+        ArrayList<String> teacherCurrentCourses1 = new ArrayList<>();
+        teacherCurrentCourses1.add("english");
+
+        Person teacher1 = new Teacher("Bob", canTeach1, teacherCurrentCourses1);
+
+        teacher1.addCourse("biology");
+        teacher1.addCourse("english");
+        teacher1.addCourse("geography");
+
+        System.out.println("Teacher: " + teacher1.getName() + "\t" + teacher1);
     }
 }
--- src/task3/Teacher.java
+++ src/task3/Teacher.java
@@ -14,6 +14,20 @@ public class Teacher extends Person {
 
     @Override
     public boolean addCourse(String course) {
-        return true;
+        if (currentCourses.contains(course)) {
+            System.out.println("Already teaching " + course + ". Course not added.");
+            return false;
+        } else if(canTeach.contains(course)){
+            System.out.println("Succesfully added " + course + " course.");
+            currentCourses.add(course);
+            return true;
+        } else  {
+            System.out.println("Can't teach " + course + ". Course not added.");
+            return false;
+        }
+    }
+
+    public String toString(){
+        return "Teachable courses: " + canTeach + "\tTeaching courses: " + currentCourses;
     }
 }
